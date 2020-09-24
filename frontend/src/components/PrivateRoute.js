ec5c3264de548a760ca3c5d1b8d27b580778aa32
Aeri <rjungdesign@gmail.com>
2020-09-24 17:25:58 -0400

    fix private route

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, Route, useHistory } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import pathList from "../pathList";
 
 function PrivateRoute({ component: Component, ...rest }) {
