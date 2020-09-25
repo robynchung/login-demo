381ab464828db4819faa77874155acc10b94a12f
Aeri <rjungdesign@gmail.com>
2020-09-24 22:47:52 -0400

    add test redirect page

--- frontend/cypress/integration/auth/sign_in.spec.js
+++ frontend/cypress/integration/auth/sign_in.spec.js
@@ -6,9 +6,10 @@ describe("Sign In", () => {
   it("input change working: ", () => {
     const username = "aeri@email.com";
     const password = "password";
-
     cy.get('input[name="username"]').type(username).should("have.value", username);
     cy.get('input[name="password"]').type(password).should("have.value", password);
     cy.get("form").submit();
+    cy.wait(2000);
+    cy.url().should("eq", "http://localhost:3000/");
   });
 });
