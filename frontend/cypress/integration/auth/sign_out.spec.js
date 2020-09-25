2e039fbd908c5090d20125dab71135ac4b61e5eb
Aeri <rjungdesign@gmail.com>
2020-09-24 23:48:20 -0400

    start sign out test

---
+++ frontend/cypress/integration/auth/sign_out.spec.js
@@ -0,0 +1,31 @@
+import { regex } from "../../../src/constants";
+
+describe("Sign Out", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("input change working: ", () => {
+    const username = "aeri@email.com";
+    const password = "password";
+
+    cy.get("[data-cy=username]").type(username).should("have.value", username);
+    cy.get("[data-cy=password]").type(password).should("have.value", password);
+    cy.get("[data-cy=submit]").submit();
+    cy.url().should("eq", "http://localhost:3000/");
+    cy.getCookie("token").should("exist");
+  });
+
+  it("username should failed: ", () => {
+    const username = "aeri-@email.com";
+    const password = "password-";
+
+    cy.get("[data-cy=username]").type(username);
+    cy.get("[data-cy=password]").type(password);
+    cy.get("[data-cy=submit]").submit();
+    cy.url().should("eq", "http://localhost:3000/sign-in");
+    cy.getCookies("token").then(cookies => {
+      expect(cookies).to.have.lengthOf(0);
+    });
+  });
+});
